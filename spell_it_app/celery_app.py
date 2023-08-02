da76c43a8e630537ba59cbd7be5c8a25606bbaa9
Artem Antonov <artyom2800@yandex.ru>
2023-08-03 04:41:25 +0500

    Move imports in celery_app and add the Sentence model

--- spell_it_app/celery_app.py
+++ spell_it_app/celery_app.py
@@ -4,7 +4,6 @@ import time
 from celery import Celery
 from django.conf import settings
 
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spell_it_app.settings')
 
 app = Celery('spell_it_app')
@@ -12,14 +11,32 @@ app.config_from_object('django.conf:settings')
 app.conf.broker_url = settings.CELERY_BROKER_URL
 app.autodiscover_tasks()
 
+if os.getenv("IS_CELERY_WORKER") == "1":
+    # download and load all models
+    print("IS_CELERY_WORKER")
+    #from bark import preload_models
+    #preload_models(text_use_small=True, coarse_use_small=True, fine_use_small=True)
 
 @app.task()
 def debug_task():
-    time.sleep(10)
+    time.sleep(5)
     print('Hello form debug_task')
 
 @app.task()
-def debug_torch_task():
-    time.sleep(5)
+def check_cuda():
     import torch
     print('Cuda availability: ', torch.cuda.is_available())
+
+@app.task()
+def bark_tts():
+    from bark import SAMPLE_RATE, generate_audio
+    from scipy.io.wavfile import write as write_wav
+    # generate audio from text
+    text_prompt = """
+        Hello, my name is Suno. And, uh — and I like pizza. [laughs] 
+        But I also have other interests such as playing tic tac toe.
+    """
+    audio_array = generate_audio(text_prompt)
+
+    # save audio to disk
+    write_wav("bark_generation.wav", SAMPLE_RATE, audio_array)
--- spell_it_app/tts/models.py
+++ spell_it_app/tts/models.py
@@ -8,4 +8,12 @@ class Word(models.Model):
 
     def __str__(self):
         return self.text
-    
+
+
+class Sentence(models.Model):
+
+    text = models.CharField(max_length=1000)
+    complexity = models.BigIntegerField(default=50)
+
+    def __str__(self):
+        return f"(complexity: {self.complexity}) {self.text}"
