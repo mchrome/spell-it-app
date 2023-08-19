c79237aa55931f9d55b0df0d15ad251f4a8777ab
Artem Antonov <artyom2800@yandex.ru>
2023-08-19 22:25:06 +0500

    Add basic interactivity

--- spell_it_app/webpage/urls.py
+++ spell_it_app/webpage/urls.py
@@ -6,4 +6,6 @@ app_name = "webpage"
 urlpatterns = [
     # ex: /webpage/
     path("", views.index, name="index"),
+    # ex: /webpage/16/submit
+    path("<int:word_id>/result", views.submit, name="result"),
 ]
--- spell_it_app/webpage/views.py
+++ spell_it_app/webpage/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 import random
 from tts.models import Word
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -15,3 +17,13 @@ def index(request):
         "random_word": random_word
     }
     return render(request=request, template_name="webpage/index.html", context=context)
+
+def submit(request, word_id):
+    if request.POST["answer"] == Word.objects.get(pk=word_id).text:
+        result_text = "Correct!"
+    else:
+        result_text = "Incorrect"
+
+    return render(request=request, 
+                  template_name="webpage/result.html", 
+                  context={"result_text": result_text})
