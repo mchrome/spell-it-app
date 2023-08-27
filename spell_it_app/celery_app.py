6dee0e89086e3e6aace56e68db09166750878dc3
Artem Antonov <artyom2800@yandex.ru>
2023-08-27 16:38:31 +0500

    Add complexity of a sentence/collection calculation

--- spell_it_app/celery_app.py
+++ spell_it_app/celery_app.py
@@ -6,6 +6,12 @@ from django.conf import settings
 from django.core.files import File
 import uuid 
 from django.db import transaction
+from django.db.models import Sum
+import string
+from django.db.models import Sum, F
+from django.db.models.expressions import Window
+from django.db.models.functions import Rank
+from math import ceil
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spell_it_app.settings')
 
@@ -53,30 +59,67 @@ def nix_tts():
 
 @app.task()
 def generate_collection(user_input: str):
-    from tts.models import Sentence, SentenceCollection
+    from tts.models import Sentence, SentenceCollection, Word
 
     collection = SentenceCollection()
     sentence_pk_list = []
     
+    complexity_sum = 0
+    collection_word_count = 0
+    # total_word_occurance = Word.objects.all().aggregate(Sum("count_frequency"))
+    # total_word_occurance = total_word_occurance["count_frequency__sum"]
+
+    total_cnt_words = Word.objects.count()    
+
     for text in user_input.split("\n"):
+
+        # Add up frequencies of words in a sentence
+        no_punctuation_sentence = text.translate(str.maketrans('', '', string.punctuation))
+        split_sentence = no_punctuation_sentence.split()
+        sentence_complexity = 0
+        for word in split_sentence:
+
+            # word_obj = Word.objects.filter(text=word).first()
+
+            word_obj = Word.objects.filter(text=word).annotate(
+                rank=Window(
+                    expression=Rank(),
+                    order_by=F('count_frequency').asc()
+                ),
+            ).first()
+
+            if word_obj:
+                sentence_complexity += word_obj.rank
+            else:
+                sentence_complexity +=  ceil(0.2 * total_cnt_words)
+        
+        complexity_sum += sentence_complexity
+        collection_word_count += len(split_sentence)
+
+        sentence_complexity = 100 - int(sentence_complexity/total_cnt_words/len(split_sentence)*100)
+
         # Generate audio for the sentence
         c, c_length, phoneme = nix.tokenize(text)
         audio = nix.vocalize(c, c_length)
+
         # Save the audio
         save_dir_path = os.path.join("tts_audio/sentence/",str(uuid.uuid4()))
         os.makedirs(os.path.join("media",save_dir_path))
         write_wav(os.path.join("media",save_dir_path, "nix_generated.wav"), 22050, audio)
+        
         # Create model instance
         # TODO: Save stripped copy of initial text
         # to compare with, when checking if user's guess is correct
         sentence = Sentence(text=text)
         sentence.audio.name = os.path.join(save_dir_path, "nix_generated.wav")
+        sentence.complexity = sentence_complexity
         sentence.save()
         sentence_pk_list.append(sentence.pk)
     
     print(sentence_pk_list)
 
     with transaction.atomic():
+        collection.complexity_score = 100 - int(complexity_sum/total_cnt_words/collection_word_count*100)
         collection.save()
         collection.sentences.add(*sentence_pk_list)    
         
